18ac31aef8a5256fdbf633adb6c1e313f625d328
Will Darkins <willdarkins@gmail.com>
2022-03-02 21:38:58 -0800

    Ran basic equation with operators then printed the value of an int variable

--- src/Hello.java
+++ src/Hello.java
@@ -1,5 +1,8 @@
 public class Hello {
     public static void main(String[] args) {
         System.out.println("Hello, Tim");
+
+        int myFirstNumber = (10 + 5) + (2 * 8);
+        System.out.println(myFirstNumber);
     }
 }
